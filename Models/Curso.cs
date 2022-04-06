406ff7b53b3b43a421c7767d1695ad2bf32a6696
Ayrton La Madrid <ayrtoncesar12@gmail.com>
2022-04-06 00:30:38 -0500

    Atributos de Validación

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -6,12 +6,15 @@ namespace platzi_asp_net_core.Models
 {
     public class Curso : ObjetoEscuelaBase
     {
-        [Required]
+        [Required(ErrorMessage = "El nombre del curso es requerido")]
+        [StringLength(5)]
         public override string Nombre {get; set;}
         public TiposJornada Jornada { get; set; }
         public List<Asignatura> Asignaturas{ get; set; }
         public List<Alumno> Alumnos{ get; set; }
-
+        [Display(Prompt = "Dirección correspondencia", Name = "Address")]
+        [Required(ErrorMessage = "Se requiere incluir una dirección")]
+        [MinLength(10, ErrorMessage = "La longitud mínima de la dirección es 10")]
         public string Dirección { get; set; }
         public string EscuelaId { get; set; }
         public Escuela Escuela { get; set; }
--- Models/EscuelaContext.cs
+++ Models/EscuelaContext.cs
@@ -83,11 +83,11 @@ namespace platzi_asp_net_core.Models
         {
             return new List<Curso>()
             {
-                new Curso() { Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "101", Jornada = TiposJornada.Mañana},
-                new Curso() { Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "201", Jornada = TiposJornada.Mañana},
-                new Curso() { Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "301", Jornada = TiposJornada.Mañana},
-                new Curso() { Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "401", Jornada = TiposJornada.Tarde},
-                new Curso() { Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "501", Jornada = TiposJornada.Tarde}
+                new Curso() { Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "101", Jornada = TiposJornada.Mañana, Dirección = "Av. Jr Comandante Guillermo Brenner"},
+                new Curso() { Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "201", Jornada = TiposJornada.Mañana, Dirección = "Av. Jr Comandante Guillermo Brenner"},
+                new Curso() { Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "301", Jornada = TiposJornada.Mañana, Dirección = "Av. Jr Comandante Guillermo Brenner"},
+                new Curso() { Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "401", Jornada = TiposJornada.Tarde, Dirección = "Av. Jr Comandante Guillermo Brenner"},
+                new Curso() { Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "501", Jornada = TiposJornada.Tarde, Dirección = "Av. Jr Comandante Guillermo Brenner"}
             };
         }
 
